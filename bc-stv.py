9122aae4b52bd6247860105a1e9b35229854cfc7
Simon Pratt <simon@pr4tt.com>
2013-10-05 17:55:11 -0400

    Implemented user lot drawing

--- bc-stv.py
+++ bc-stv.py
@@ -2,6 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# This script expects the first command-line argument to be a filename
+# of a json file whose format is an array of objects which map
+# candidates (strings) to preference values (numbers); and it expects
+# the second c.l. arg. to be the number of seats.
+
 import math
 import json
 import sys
@@ -27,38 +32,66 @@ class Ballot:
         # If ranks are duplicated/skipped, accept ranks until the first error.
         # XXX This is ugly.
         obj = {k: v for k, v in obj.items() if v != 0} # Filter out 0 ranks.
-        if len(obj) == 0: return Ballot([])
+        if len(obj) == 0: return None
         ranks, prefs = zip(*sorted(zip(obj.values(), obj.keys())))
         assert len(ranks) == len(set(ranks)) # XXX Should filter out duplicates
+        if ranks[0] != 1: return None
         ranked_prefs = zip(itertools.count(1), ranks, prefs)
         prefs = [pref for i, rank, pref in ranked_prefs if i == rank]
         return Ballot(prefs)
 
 def main():
     path = sys.argv[1]
+    seats = int(sys.argv[2])
     with open(path, 'r') as f:
         j = json.load(f)
     # Assume j is a list of mappings [{name->pref-value}]
     bc = BC_STV(j)
-    bc.bc_stv(7)
+    bc.bc_stv(seats)
     print(bc.elected)
 
 class BC_STV:
     def __init__(self, data):
-        self.ballots = list(map(Ballot.from_ranks, data))
+        self.ballots = [x for x in list(map(Ballot.from_ranks, data)) if x != None]
         self.elected = []
+        self.counts = []
 
     def bc_stv(self, seats):
-        self.quota = math.floor(len(self.ballots) / (seats + 1)) + 1
+        self.quota = math.floor((len(self.ballots) / (seats+1))+1)
         print("Threshold: " + str(self.quota))
         while len(self.elected) < seats:
             self.redistribute()
             mx = max(self.piles.items(), key=lambda kv: value(kv[1]))
-            mn = min(self.piles.items(), key=lambda kv: value(kv[1]))
-            if value(mx[1]) >= self.quota:
+            mn = self.min()
+            self.counts.append(zip(map(lambda x: x[0], self.piles.items()),
+                                   map(lambda x: value(x[1]), self.piles.items())))
+            if value(mx[1]) >= self.quota or len(self.piles) == seats - len(self.elected):
                 self.elect(mx[0])
             else:
-                self.eliminate(mn[0])
+                self.eliminate(mn)
+
+    def min(self):
+        min_val = min([value(l[1]) for l in self.piles.items()])
+        mins = [item[0] for item in self.piles.items() if min_val == value(item[1])]
+        counts = len(self.counts)
+        for count in reversed(self.counts):
+            count = list(filter(lambda x: x[0] in mins, count))
+            if len(count) < len(mins):
+                for cand in mins:
+                    if cand not in list(map(lambda x: x[0], count)):
+                        count.append((cand,0))
+            count_vals = [l[1] for l in count]
+            min_val = min(count_vals)
+            mins = [item[0] for item in count if min_val == item[1]]
+            if len(mins) == 1:
+                return mins[0]
+        if len(mins) == 1:
+            return mins[0]
+        choice = ""
+        while choice not in mins:
+            print("Please draw lots to eliminate a candidate [{}]: ".format(mins),end="")
+            choice = input()
+        return choice
 
     def redistribute(self):
         active_ballots = [b for b in self.ballots if b.is_active()]
@@ -83,7 +116,6 @@ class BC_STV:
         for lst in self.piles.values():
             for ballot in lst:
                 ballot.eliminate(candidate)
-
 
 def value(ballots):
     return sum(ballot.value for ballot in ballots)
